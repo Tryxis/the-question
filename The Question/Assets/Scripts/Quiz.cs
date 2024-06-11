d93837df3264b1b4d1f5086e2a82320ed558ead0
Tryxis <47526388+Tryxis@users.noreply.github.com>
2024-06-11 13:09:36 +0100

    Sprite swaps and canvas text updates on answer

--- The Question/Assets/Scripts/Quiz.cs
+++ The Question/Assets/Scripts/Quiz.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtions;
+    [SerializeField] int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
 
     void Start()
     {
@@ -20,4 +24,24 @@ public class Quiz : MonoBehaviour
         }
     }
 
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+
+        if(index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct";
+            buttonImage = answerButtions[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Incorrect - the correct answer is \n" + correctAnswer;
+
+            buttonImage = answerButtions[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+    }
 }
